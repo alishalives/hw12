0e045c6b25d06aa837f31268f33b387b2597a152
ADMIN <alina.velichko1@mail.ru>
2023-01-23 15:19:15 +0300

    updated files

--- loader/utils.py
+++ loader/utils.py
@@ -6,17 +6,20 @@ from main.utils import read_posts
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 
+# Функция, сохраняющая изображение при правильном расширении
 def save_picture(picture):
     extension = picture.filename.split(".")[-1]
+    filename = picture.filename
     if extension in ALLOWED_EXTENSIONS:
-        picture.save(f"./uploads/{picture.filename}")
-        return f"/uploads/{picture.filename}"
+        picture.save(f"./uploads/images/{filename}")
+        return filename
     else:
         return
 
 
+# Функция перезаписывает json файл с учетом нового поста
 def save_to_json(posts_file, picture, content):
-    post = {'pic': save_picture(picture), 'content': content}
+    post = {'pic': f"/uploads/images/{picture.filename}", 'content': content}
     posts = read_posts(posts_file)
     posts.append(post)
     with open(posts_file, 'w', encoding="utf-8") as file:
